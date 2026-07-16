79d109122221d2c00a41f84909631cbdbaed191b
Ajay4145 <ajaytamang4145@gmail.com>
2026-07-16 12:46:12 +0545

    Add Counter and HookCounter components; update HomePage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import CourseCard from "@/components/CourseCard";
 import Hero from "@/components/Hero";
+import Counter from "@/components/Counter";
+import HookCounter from "@/components/HookCounter";
 
 export default function HomePage() {
   return (
     <>
       <h1>Home Page</h1>
-
       <Hero />
       <CourseCard />
+      <Counter />
+      <HookCounter />
     </>
   );
 }
